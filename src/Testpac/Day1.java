33dc194e3f4c348507438e445373404eb17cdeff
satya <yvvsatyanarayana2014@gmail.com>
2021-11-10 22:26:11 +0530

    first commit

---
+++ src/Testpac/Day1.java
@@ -0,0 +1,20 @@
+package Testpac;
+
+import org.testng.annotations.Test;
+
+public class Day1 {
+
+
+@Test
+public void Demo()
+{
+	System.out.println("hello");
+}
+
+@Test
+public void Demo1()
+{
+	System.out.println("Bye");
+}
+
+}
